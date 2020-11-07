37a732c02105ae712d51243c70d60bb1264ffdc9
floaterest <56704092+floaterest@users.noreply.github.com>
2020-11-07 16:29:10 -0500

    Add song list, add 17sai and setsugetsuka

--- kashi/kashi.js
+++ kashi/kashi.js
@@ -1,13 +1,22 @@
 
 window.onload = function() {
 	$.getJSON('lyrics/data.json', function (data) {
-        var song = data[0];
-        $.get('lyrics/'+song, function(ly){
-            // convert txt to html
-            ly = ly.replaceAll('\n','<br>\n');
-            ly = ly.replace(/([\u4e00-\u9faf]+)\(([\u3040-\u309f]+)\)/g,
-                            '<span class="ruby"><span class="rb">$1</span><span class="rt">$2</span></span>');
-            $('#lyric').html(ly);
-        });
+        for(let song of data){
+            $('#menu').prepend(
+                $('<a>').attr('href',`#${song}`)
+                        .text(song)
+                        .click(function(){
+                            $('#lyric').empty();
+                            $.get('lyrics/'+this.innerText, function(lyric){
+                                // convert txt to html
+                                lyric = lyric.replaceAll('\n','<br>\n');
+                                lyric = lyric.replace(/([\u4e00-\u9faf]+)\(([\u3040-\u309f]+)\)/g,
+                                                '<ruby><rb>$1</rb><rt>$2</rt></ruby>');
+                                $('#lyric').html(lyric);
+                            });
+
+                        })
+            );
+        }
 	});
 }
