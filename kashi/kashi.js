6bec4167d3bdf691cdd2ee478495c0119acf2540
floaterest <56704092+floaterest@users.noreply.github.com>
2020-11-07 17:03:05 -0500

    Add toogle furigana

--- kashi/kashi.js
+++ kashi/kashi.js
@@ -6,15 +6,26 @@ window.onload = function() {
                 $('<a>').attr('href',`#${song}`)
                         .text(song)
                         .click(function(){
-                            $('#lyric').empty();
-                            $.get('lyrics/'+this.innerText, function(lyric){
-                                // convert txt to html
-                                lyric = lyric.replaceAll('\n','<br>\n');
-                                lyric = lyric.replace(/([\u4e00-\u9faf]+)\(([\u3040-\u309f]+)\)/g,
-                                                '<ruby><rb>$1</rb><rt>$2</rt></ruby>');
-                                $('#lyric').html(lyric);
-                            });
+                            // if clicked on seleted song
+                            if(this.classList.contains('selected')){
+                                $('rt').each(function() {
+                                    this.classList.toggle('hidden');
+                                });
+                            }else{
+                                // remove all 'selected' class
+                                $('a').attr('class','');
+                                // add 'selected' class to current element
+                                this.classList.add('selected');
 
+                                $('#lyric').empty();
+                                $.get('lyrics/'+this.innerText, function(lyric){
+                                    // convert txt to html
+                                    lyric = lyric.replaceAll('\n','<br>\n');
+                                    lyric = lyric.replace(/([\u4e00-\u9faf]+)\(([\u3040-\u309f]+)\)/g,
+                                                    '<ruby><rb>$1</rb><rt>$2</rt></ruby>');
+                                    $('#lyric').html(lyric);
+                                });
+                            }
                         })
             );
         }
