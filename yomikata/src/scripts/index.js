b79f80f739bb08e0b33938b155f29d927b3e7321
floaterest <56704092+floaterest@users.noreply.github.com>
2021-05-05 10:04:11 -0400

    feat: add raw

--- yomikata/src/scripts/index.js
+++ yomikata/src/scripts/index.js
@@ -2,15 +2,23 @@ const HTMLClass = {
 	Loading: 'loading',
 };
 
-var w = new Worker('./src/scripts/worker.js');
+let html = document.getElementById('html');
+let raw = document.getElementById('raw');
+
+let w = new Worker('./src/scripts/worker.js');
 w.onmessage = function(e){
 	switch(e.data.type){
 		case 'done':
 			return document.body.classList.remove(HTMLClass.Loading);
 		case 'ruby':
-			return document.getElementById('html').innerHTML = e.data.data;
+			html.innerHTML = raw.innerText = e.data.data;
+			break;
 	}
 };
+
+document.getElementById('input').addEventListener('input', e => {
+	w.postMessage(e.target.value);
+});
 // w.postMessage('そう出逢う前から解ってた');
 // w.onmessage = function(e){
 // 	document.body.innerHTML = e.data;
