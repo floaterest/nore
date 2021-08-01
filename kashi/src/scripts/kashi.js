7d56c2471f4f66e9f1d0c125704f58e998c26898
Floaterest <floaterest@outlook.jp>
2021-08-01 17:35:34 -0400

    feat: add toggle and switch

--- kashi/src/scripts/kashi.js
+++ kashi/src/scripts/kashi.js
@@ -17,38 +17,25 @@ function isSelecting() {
     return document.getSelection().type != 'Caret';
 }
 var Kashi = /** @class */ (function () {
-    function Kashi(elements) {
+    function Kashi(content) {
         this.isSwitched = false;
         this.isToggled = false;
-        this.rubySelector = elements.rubySelector;
-        this.$switch = elements.switch.text(this.getSwitchText());
-        this.$toggle = elements.toggle.text(this.getToggleText());
+        this.$ruby = content.find('ruby');
+        this.$rt = this.$ruby.find('rt');
     }
-    Kashi.prototype.getRuby = function () {
-        return $(this.rubySelector);
-    };
-    Kashi.prototype.getSwitchText = function () {
-        return SWITCH[+this.isSwitched];
-    };
-    Kashi.prototype.getToggleText = function () {
-        return TOGGLE[+this.isToggled];
-    };
     /**
      * switch rt and rb
      */
     Kashi.prototype.switch = function () {
         this.isSwitched = !this.isSwitched;
-        var $ruby = this.getRuby();
-        $ruby.each(function () {
+        this.$ruby.each(function () {
             this.innerHTML = this.innerHTML.replace(/(\S+)(<rt.*>)(\S+)/, '$3$2$1');
         });
-        this.$switch.text(this.getSwitchText());
-        toggleClass(this.isSwitched, $ruby, HTMLClass.Underline);
+        toggleClass(this.isSwitched, this.$ruby, HTMLClass.Underline);
     };
     Kashi.prototype.toggle = function () {
         this.isToggled = !this.isToggled;
-        var $ruby = this.getRuby();
-        toggleClass(this.isToggled, $ruby, HTMLClass.Hidden);
+        toggleClass(this.isToggled, this.$rt, HTMLClass.Hidden);
     };
     return Kashi;
 }());
--- kashi/src/scripts/kashi.ts
+++ kashi/src/scripts/kashi.ts
@@ -20,26 +20,12 @@ class Kashi{
     isSwitched = false;
     isToggled = false;
 
-    rubySelector: string;
-    $switch: JQuery;
-    $toggle: JQuery;
+    $ruby: JQuery;
+    $rt: JQuery;
 
-    constructor(elements: { rubySelector: string, switch: JQuery, toggle: JQuery }){
-        this.rubySelector = elements.rubySelector;
-        this.$switch = elements.switch.text(this.getSwitchText());
-        this.$toggle = elements.toggle.text(this.getToggleText());
-    }
-
-    getRuby(): JQuery{
-        return $(this.rubySelector);
-    }
-
-    getSwitchText(): string{
-        return SWITCH[+this.isSwitched];
-    }
-
-    getToggleText(): string{
-        return TOGGLE[+this.isToggled];
+    constructor(content: JQuery){
+        this.$ruby = content.find('ruby');
+        this.$rt = this.$ruby.find('rt');
     }
 
     /**
@@ -47,18 +33,15 @@ class Kashi{
      */
     switch(){
         this.isSwitched = !this.isSwitched;
-        let $ruby = this.getRuby();
-        $ruby.each(function(){
+        this.$ruby.each(function(){
             this.innerHTML = this.innerHTML.replace(/(\S+)(<rt.*>)(\S+)/, '$3$2$1');
         });
 
-        this.$switch.text(this.getSwitchText());
-        toggleClass(this.isSwitched, $ruby, HTMLClass.Underline);
+        toggleClass(this.isSwitched, this.$ruby, HTMLClass.Underline);
     }
 
     toggle(){
         this.isToggled = !this.isToggled;
-        let $ruby = this.getRuby();
-        toggleClass(this.isToggled, $ruby, HTMLClass.Hidden);
+        toggleClass(this.isToggled, this.$rt, HTMLClass.Hidden);
     }
 }
