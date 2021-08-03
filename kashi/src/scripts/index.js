030da4a9dc6db355f850973fce3629489f4ce558
Floaterest <floaterest@outlook.jp>
2021-08-03 12:34:22 -0400

    style: change buttons text

--- kashi/src/scripts/index.js
+++ kashi/src/scripts/index.js
@@ -35,8 +35,8 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var SWITCH = ['⇅', '⇵'];
-var TOGGLE = ['０', 'ー'];
+var SWITCH = ['⇈', '⇊'];
+var TOGGLE = ['0', '-'];
 var c = $('#content');
 var t = $('#toc');
 var d = 'src/lyrics/';
