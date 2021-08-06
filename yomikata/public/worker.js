26d0c00cb9f677bde3ab000c4f36e61b899f254b
Floaterest <floaterest@outlook.jp>
2021-08-06 08:19:43 -0400

    feat(*): works

---
+++ yomikata/public/worker.js
@@ -0,0 +1,19 @@
+importScripts('kuromoji.js');
+
+
+let token;
+kuromoji.builder({dicPath: './dict'}).build((err, t) => {
+	token = t;
+	postMessage(true);
+});
+
+/**
+ * @param lines [string, boolean][][]
+ */
+function tokenize(lines){
+	return lines.map(l => l.map(b => b[1] ? token.tokenize(b[0]) : b[0]));
+}
+
+onmessage = e => {
+	postMessage(tokenize(e.data));
+};
