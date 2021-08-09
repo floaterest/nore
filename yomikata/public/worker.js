1683bffc82b462a165817da8a49e413ebe299d10
Floaterest <floaterest@outlook.jp>
2021-08-09 14:05:49 -0400

    refactor(conv,worker): refactor and add console.debug

--- yomikata/public/worker.js
+++ yomikata/public/worker.js
@@ -11,9 +11,8 @@ kuromoji.builder({dicPath: './dict'}).build((err, t) => {
  * @param lines [string, boolean][][]
  */
 function tokenize(lines){
+	console.debug('worker received', lines);
 	return lines.map(l => l.map(b => b[1] ? token.tokenize(b[0]) : b[0]));
 }
 
-onmessage = e => {
-	postMessage(tokenize(e.data));
-};
+onmessage = e => postMessage(tokenize(e.data));
