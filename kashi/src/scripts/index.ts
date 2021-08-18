05cdabc303c1db8298a5c916c1771fb2c5b310f0
floaterest <56704092+floaterest@users.noreply.github.com>
2021-08-18 09:59:45 -0400

    refactor(ts): move functions

--- kashi/src/scripts/index.ts
+++ kashi/src/scripts/index.ts
@@ -8,39 +8,6 @@ let directory = 'src/lyrics/';
 let selected = '';
 let kashi: Kashi;
 
-function update(content: string): JQuery{
-    $content.html(content).find('ruby').on('click', function(){
-        if(isSelecting()) return;
-        $(this).each(function(){
-            this.classList.toggle(HTMLClass.Hidden);
-        });
-    });
-    return $content;
-}
-
-/**
- * generate a new item for the table of contents
- * @param text innerText for this element
- * @param path file to download when clicked
- */
-function item(text: string, path: string): JQuery{
-    return $('<p>').on('click', async function(){
-        // if clicked on selected, refresh the content
-        let refresh = this.innerText == selected;
-        let content = sessionStorage.getItem(path);
-        if(refresh || content == null){
-            // download file, set session storage, assign to content
-            await $.get(path, f => sessionStorage.setItem(path, content = f));
-        }
-        kashi = new Kashi(update(content!));
-
-        selected = this.innerText;
-        document.body.classList.remove(HTMLClass.HideContent);
-        window.scrollTo(0, 0);
-
-    }).text(text);
-}
-
 $.getJSON('src/lyrics.json').done((data: string[]) => {
     for(const line of data){
         $toc.prepend(item(line, directory + line + '.html'));
@@ -61,4 +28,4 @@ $('#toggle').text(TOGGLE[0]).on('click', function(){
     this.innerText = TOGGLE[+kashi.isToggled];
 });
 
-$('#hamburger').text(HAMBURGER).on('click',()=>document.body.classList.toggle(HTMLClass.HideContent));
+$('#hamburger').text(HAMBURGER).on('click', () => document.body.classList.toggle(HTMLClass.HideContent));
