8b574035fc846d36f51a47cdad7f41fff46d2d39
Floaterest <floaterest@outlook.jp>
2021-08-18 09:59:45 -0400

    refactor(ts): move functions

--- kashi/src/scripts/kashi.ts
+++ kashi/src/scripts/kashi.ts
@@ -16,6 +16,40 @@ function isSelecting(){
     return document.getSelection()!.type != 'Caret';
 }
 
+function update(content: string): JQuery{
+    $content.html(content).find('ruby').on('click', function(){
+        if(isSelecting()) return;
+        $(this).each(function(){
+            this.classList.toggle(HTMLClass.Hidden);
+        });
+    });
+    return $content;
+}
+
+/**
+ * generate a new item for the table of contents
+ * @param text innerText for this element
+ * @param path file to download when clicked
+ */
+function item(text: string, path: string): JQuery{
+    return $('<p>').on('click', async function(){
+        // if clicked on selected, refresh the content
+        let refresh = this.innerText == selected;
+        let content = sessionStorage.getItem(path);
+        if(refresh || content == null){
+            // download file, set session storage, assign to content
+            await $.get(path, f => sessionStorage.setItem(path, content = f));
+        }
+        kashi = new Kashi(update(content!));
+
+        selected = this.innerText;
+        document.body.classList.remove(HTMLClass.HideContent);
+        window.scrollTo(0, 0);
+
+    }).text(text);
+}
+
+
 class Kashi{
     isSwitched = false;
     isToggled = false;
