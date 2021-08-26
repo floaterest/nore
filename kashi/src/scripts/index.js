1af811e7360845e95e6ffafafa0580c25769ef68
floaterest <56704092+floaterest@users.noreply.github.com>
2021-08-26 17:47:08 -0400

    refactor(index.ts)

--- kashi/src/scripts/index.js
+++ kashi/src/scripts/index.js
@@ -1,8 +1,36 @@
 "use strict";
-var SWITCH = ['▲', '▼'];
-var TOGGLE = ['O', 'Ø'];
-var EDIT = ['X', 'I'];
-var HAMBURGER = 'Ξ';
+var Controls = {
+    '#switch': {
+        icon: ['▲', '▼'],
+        action: function (e) {
+            if (!kashi)
+                return;
+            kashi.switch();
+            e.target.innerText = Controls['#switch'].icon[+kashi.isSwitched];
+        },
+    },
+    '#edit': {
+        icon: ['X', 'I'],
+        action: function (e) {
+            var editable = $content.attr('contenteditable') !== 'true';
+            $content.attr('contenteditable', editable.toString());
+            e.target.innerText = Controls['#edit'].icon[+editable];
+        },
+    },
+    '#toggle': {
+        icon: ['O', 'Ø'],
+        action: function (e) {
+            if (!kashi)
+                return;
+            kashi.toggle();
+            e.target.innerText = Controls['#toggle'].icon[+kashi.isToggled];
+        },
+    },
+    '#hamburger': {
+        icon: ['Ξ'],
+        action: function () { return document.body.classList.toggle(HTMLClass.HideContent); },
+    },
+};
 var INDEX = 'src/lyrics.json';
 var DIRECTORY = 'src/lyrics/';
 var QUERIES = {
@@ -61,22 +89,8 @@ $.getJSON(INDEX).done(function (data) {
         }
     }
 });
-$('#switch').text(SWITCH[0]).on('click', function () {
-    if (!kashi)
-        return;
-    kashi.switch();
-    this.innerText = SWITCH[+kashi.isSwitched];
-});
-$('#edit').text(EDIT[0]).on('click', function () {
-    var editable = !$content.attr('contenteditable');
-    $content.attr('contenteditable', editable.toString());
-    this.innerText = EDIT[+editable];
-});
-$('#toggle').text(TOGGLE[0]).on('click', function () {
-    if (!kashi)
-        return;
-    kashi.toggle();
-    this.innerText = TOGGLE[+kashi.isToggled];
-});
-$('#hamburger').text(HAMBURGER).on('click', function () { return document.body.classList.toggle(HTMLClass.HideContent); });
+for (var _i = 0, _a = Object.entries(Controls); _i < _a.length; _i++) {
+    var _b = _a[_i], selector = _b[0], data = _b[1];
+    $(selector).text(data.icon[0]).on('click', data.action);
+}
 //# sourceMappingURL=index.js.map
