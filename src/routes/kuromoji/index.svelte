5b006b1fb354266a7eaf248a9fb28632ffe3e06d
floaterest <56704092+floaterest@users.noreply.github.com>
2021-09-10 13:22:41 -0400

    feat(kuromoji): parse raw html

---
+++ src/routes/kuromoji/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Textfield from '@smui/textfield';
+
+    let raw = localStorage.getItem('kuromoji-raw') || '';
+
+    $:localStorage.setItem('kuromoji-raw', raw);
+</script>
+
+<Textfield variant="outlined" bind:value={raw} label="raw"/>
+
+<section class="mdc-typography--body1">
+    {@html raw}
+</section>
