aa089ea59282fcbcc12195719abd20999541cc00
Floaterest <floaterest@outlook.jp>
2021-09-10 13:08:50 -0400

    chore: theme

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,1 +1,14 @@
+<script lang="ts">
+    import Button, { Group, Label } from '@smui/button';
+</script>
+
+<Group style="display: flex;">
+    <Button href="/kuromoji">
+        <Label>Kuromoji</Label>
+    </Button>
+</Group>
+
 <slot/>
+
+<style>
+</style>
