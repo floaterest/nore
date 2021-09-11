9eb008f805d8ae7400695e87380994b3a91c9294
floaterest <56704092+floaterest@users.noreply.github.com>
2021-09-11 15:59:26 -0400

    feat(kuromoji): toggle rt

--- src/routes/kuromoji/index.svelte
+++ src/routes/kuromoji/index.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    import Textfield from '@smui/textfield';
     import Layout from '$lib/Layout.svelte';
 
+    import Textfield from '@smui/textfield';
+    import IconButton, { Icon } from '@smui/icon-button';
     // @ts-ignore
     import { browser } from '$app/env';
 
@@ -14,16 +15,28 @@
     $: if(browser){
         localStorage.setItem('kuromoji', JSON.stringify(kuromoji));
     }
+
+    let visible = true;
 </script>
 
 <Layout title="Kuromoji">
-
+    <IconButton toggle bind:pressed={visible}>
+        <Icon class="material-icons" on>visibility</Icon>
+        <Icon class="material-icons">visibility_off</Icon>
+    </IconButton>
 </Layout>
 
 
 <section class="mdc-typography--body1">
     <Textfield variant="outlined" bind:value={kuromoji.raw} label="raw"/>
     <p>
+        {#if !visible}
+            <style>
+                rt{
+                    display: none;
+                }
+            </style>
+        {/if}
         {@html kuromoji.raw}
     </p>
 </section>
