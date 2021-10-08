e980720106d604a3fe4238e9d2d7a717d62ad0c5
Floaterest <floaterest@outlook.jp>
2021-10-08 15:10:36 -0400

    add kuromoji.svelte

---
+++ src/routes/kuromoji.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    //@ts-ignore
+    import { browser } from '$app/env';
+    import Layout from '$lib/Layout.svelte';
+    import File from '$lib/File.svelte';
+
+    import Textfield from '@smui/textfield';
+    import IconButton, { Icon } from '@smui/icon-button';
+    import SegmentedButton, { Segment } from '@smui/segmented-button';
+
+    // raw (and fresh) html string from storage
+    let raw = browser ? 'help' : '';
+    let files;
+</script>
+
+<Layout>
+    <IconButton title="Redirect to Ruby" href="/ruby">
+        <span class="material-icons">code</span>
+    </IconButton>
+</Layout>
+
+<main>
+    <section>
+        <File label="upload text" {files}/>
+        <Textfield textarea label="text/plain" variant="outlined" spellcheck="false" bind:value={raw}/>
+    </section>
+    <section></section>
+</main>
