e3d4cd9a239c192c0ef07d48c190791ec2c79438
Floaterest <floaterest@outlook.jp>
2021-10-09 09:31:35 -0400

    refactor(File,ruby,kuro): move file reader

--- src/lib/File.svelte
+++ src/lib/File.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
     export let label: string;
-    export let files: string[];
+    export let content: string = '';
+
+    let files: Blob[];
+    $: if(files && files[0]){
+        const reader = new FileReader();
+        reader.onload = function(){
+            content = this.result as string;
+            files = [];
+        };
+        reader.readAsText(files[0], 'utf8');
+    }
 </script>
 
 <label for="file" class="mdc-button mdc-button--outlined">
