a7401aff664bac46df352f30732855ba407a8703
Floaterest <floaterest@outlook.jp>
2021-10-17 11:51:27 -0400

    chore(yomitaka): delete

--- yomikata/src/stores.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const data = writable(
-    'ほう、' +
-    '<abbr title="delightful">' +
-    '<a href="https://www.weblio.jp/content/微笑ましい">微笑ましい</a>' +
-    '</abbr>じゃないか',
-);
-export const output = writable('');
