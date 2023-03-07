5b3e18de6188b64c54e1ea07832ed8ac5f49a370
floaterest <floaterest@users.noreply.github.com>
2023-03-07 18:34:06 -0500

    chore(test/ruby) concurrent

--- test/ruby.test.ts
+++ test/ruby.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { extract } from '../src/lib/ruby';
 
 describe('ruby injection', () => {
-    it('prefix and suffix', async () => {
+    it.concurrent('prefix and suffix', async () => {
         const actual = extract('お知らせ', 'おしらせ');
         expect(actual).toStrictEqual(['お', ['知', 'し'], 'らせ']);
     });
