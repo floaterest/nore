d97d0f038bdd127ec414ec717242538183d3bdb5
floaterest <floaterest@users.noreply.github.com>
2023-03-06 20:09:52 -0500

    refactor(ruby.test) test name

--- src/lib/ruby.test.ts
+++ src/lib/ruby.test.ts
@@ -1,8 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import { extract } from './ruby';
 
-describe('test ruby API', () => {
-    it('tests', async () => {
+describe('ruby injection', () => {
+    it('prefix and suffix', async () => {
         const actual = extract('お知らせ', 'おしらせ');
         expect(actual).toStrictEqual(['お', ['知', 'し'], 'らせ']);
     });
