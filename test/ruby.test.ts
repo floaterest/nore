b72877af8d8e416f0aaa09e33b05ffaa18304abf
floaterest <floaterest@users.noreply.github.com>
2023-03-07 19:09:46 -0500

    chore(test/ruby) suffix only

--- test/ruby.test.ts
+++ test/ruby.test.ts
@@ -6,4 +6,8 @@ describe('ruby injection', () => {
         const actual = extract('お知らせ', 'おしらせ');
         expect(actual).toStrictEqual(['お', ['知', 'し'], 'らせ']);
     });
+    it.concurrent('suffix only', () => {
+        const actual = extract('輝かしい', 'かがやかしい');
+        expect(actual).toStrictEqual(['', ['輝', 'かがや'], 'かしい']);
+    });
 });
