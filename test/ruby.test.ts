867556071f28a63c35731ab6dd68e172e127cc64
floaterest <floaterest@users.noreply.github.com>
2023-03-15 19:57:39 -0400

    test(ruby) multiple suites

--- test/ruby.test.ts
+++ test/ruby.test.ts
@@ -1,23 +1,27 @@
 import { describe, it, expect } from 'vitest';
 import { extract } from '../src/lib/ruby';
 
-describe('ruby injection', () => {
-    it.concurrent('prefix and suffix', () => {
-        const actual = extract('お知らせ', 'おしらせ');
-        expect(actual).toStrictEqual(['お', ['知', 'し'], 'らせ']);
-    });
-    it.concurrent('suffix only', () => {
-        const actual = extract('逆さ', 'さかさ');
-        expect(actual).toStrictEqual([['逆', 'さか'], 'さ']);
-    });
-    it.concurrent('prefix only', () => {
-        const actual = extract('ご飯', 'ごはん');
-        expect(actual).toStrictEqual(['ご', ['飯', 'はん']]);
-    });
-    it.concurrent('no kana', () => {
+describe('pure', () => {
+    it.concurrent('kanji', () => {
         const actual = extract('丁々発止', 'ちょうちょうはっし');
         expect(actual).toStrictEqual([['丁々発止', 'ちょうちょうはっし']]);
     });
+});
+
+describe('mixed', () => {
+    it.concurrent('kana as prefix and suffix', () => {
+        const actual = extract('お知らせ', 'おしらせ');
+        expect(actual).toStrictEqual(['お', ['知', 'し'], 'らせ']);
+    });
+    it.concurrent('kana as suffix only', () => {
+        const actual = extract('逆さ', 'さかさ');
+        expect(actual).toStrictEqual([['逆', 'さか'], 'さ']);
+    });
+    it.concurrent('kana as prefix only', () => {
+        const actual = extract('ご飯', 'ごはん');
+        expect(actual).toStrictEqual(['ご', ['飯', 'はん']]);
+    });
+
     it.concurrent('kana in middle', () => {
         const actual = extract('案の定', 'あんのじょう');
         expect(actual).toStrictEqual([['案', 'あん'], 'の', ['定', 'じょう']]);
