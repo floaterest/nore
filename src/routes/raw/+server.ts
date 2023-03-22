cabc217ee741cd1d10eae2f25d0675406bb530ff
floaterest <floaterest@users.noreply.github.com>
2023-03-22 18:35:47 -0400

    refactor(raw/server) satisfies

--- src/routes/raw/+server.ts
+++ src/routes/raw/+server.ts
@@ -5,4 +5,4 @@ export const POST = (async ({request}) => new Response(
 <html><head><meta charset="utf-8"></head>
 <body style="white-space: pre">${(await request.text()).replace(/^body=/, '')}</body>
 </html>`, { headers: { 'content-type': 'text/html' } })
-) as RequestHandler;
+) satisfies RequestHandler;
